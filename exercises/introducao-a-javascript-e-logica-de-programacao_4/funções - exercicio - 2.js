8f2e00fa148b67b829f8446fc9ff9f0f5cdb0e7e
Maximilian Kaden <mkadenfilgo@gmail.com>
2020-11-30 19:04:03 -0300

    Atualizado exercicio 3 e feito exercicio 4

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/funções - exercicio - 2.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/funções - exercicio - 2.js
@@ -1,16 +1,16 @@
 let dados =  [2, 4, 6, 7, 10, 0, -3]
 
-getIndexLowerNumber(dados)
+getIndexHigherNumber(dados)
 
-function getIndexLowerNumber (array){
+function getIndexHigherNumber (array){
   console.log(array)
-  let indexLowerNumber = 0;
+  let indexHigherNumber = 0;
   for (index in array){
     console.log(array[index])
-    if (array[index] < array[indexLowerNumber]){
-      console.log(`O ${array[index]} é maior que ${array[indexLowerNumber]}`)
-      indexLowerNumber = index
+    if (array[index] > array[indexHigherNumber]){
+      console.log(`O ${array[index]} é maior que ${array[indexHigherNumber]}`)
+      indexHigherNumber = index
     }
   }
-  console.log(`O index do maior numero é ${indexLowerNumber}, que vale o numnero ${array[indexLowerNumber]}`)
+  console.log(`O index do maior numero é ${indexHigherNumber}, que vale o numnero ${array[indexHigherNumber]}`)
 }
